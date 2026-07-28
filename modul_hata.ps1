ccaf0a494201ed05b53d1405e48c71c7beb14295
CodeAnd-Art <3838yos4141@gmail.com>
2026-07-28 08:25:28 +0300

    Delete modul_hata.ps1

--- modul_hata.ps1
+++
@@ -1,8 +0,0 @@
-$log = "$env:USERPROFILE\Desktop\Carboxyhemoglobin_log.txt"
-while ($true) {
-    try {
-        $zaman = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-        Add-Content -Path $log -Value "$zaman - System failure simulated"
-    } catch {}
-    Start-Sleep -Seconds 15
-}
