5d7932388384d9132d0cf72161f32fddfec97a09
CodeAnd-Art <3838yos4141@gmail.com>
2026-07-28 08:26:06 +0300

    Delete modul_tarayici.ps1

--- modul_tarayici.ps1
+++
@@ -1,9 +0,0 @@
-while ($true) {
-    if ((Get-Random -Min 1 -Max 15) -eq 1) {
-        try {
-            Start-Process "https://www.google.com/search?q=kernel+panic"
-            Write-Host "🌐 Kernel panic search opened" -ForegroundColor Yellow
-        } catch {}
-    }
-    Start-Sleep -Seconds 60
-}
